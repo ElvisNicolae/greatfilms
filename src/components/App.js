e499302cf9c54d00cf094036d5617865c8ee216a
ElvisNicolae <budicanicolaeelvis@gmail.com>
2021-10-09 20:24:53 +0300

    added the necesarry things for github pages to work and made small adjustments

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import HomePage from './HomePage/HomePage';
-import MoviePage from './MoviePage/MoviePage';
-import TopRatedPage from './TopRatedPage/TopRatedPage';
-import TrendingPage from './TrendingPage/TrendingPage';
-import UpcomingPage from './UpcomingPage/UpcomingPage';
-import SearchPage from './SearchPage/SearchPage';
-import RequestToken from './Authentication/RequestToken';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import Login from './Authentication/Login/Login';
-import WatchlistPage from './WatchlistPage/WatchlistPage';
-import PopularPeoplePage from './PopularPeoplePage/PopularPeoplePage';
-import PersonPage from './PersonPage/PersonPage';
-import ListPage from './ListPage/ListPage';
+import { BrowserRouter, Route } from "react-router-dom";
+import HomePage from "./HomePage/HomePage";
+import MoviePage from "./MoviePage/MoviePage";
+import TopRatedPage from "./TopRatedPage/TopRatedPage";
+import TrendingPage from "./TrendingPage/TrendingPage";
+import UpcomingPage from "./UpcomingPage/UpcomingPage";
+import SearchPage from "./SearchPage/SearchPage";
+import RequestToken from "./Authentication/RequestToken";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Login from "./Authentication/Login/Login";
+import WatchlistPage from "./WatchlistPage/WatchlistPage";
+import PopularPeoplePage from "./PopularPeoplePage/PopularPeoplePage";
+import PersonPage from "./PersonPage/PersonPage";
+import ListPage from "./ListPage/ListPage";
 import "./App.scss";
 
 function App() {
   return (
     <div id="app">
-      <BrowserRouter>
+      <BrowserRouter basename="/greatfilms">
         <Header />
         <Route path="/" exact component={HomePage} />
         <Route path="/movie/:id" component={MoviePage} />
@@ -28,10 +28,10 @@ function App() {
         <Route path="/search/:term" component={SearchPage} />
         <Route path="/approved" component={RequestToken} />
         <Route path="/login" component={Login} />
-        <Route path="/movies/watchlist" component={WatchlistPage}/>
-        <Route path="/people/:id" component={PersonPage}/>
-        <Route path="/list/:id" component={ListPage}/>
-        <Route path="/people" exact component={PopularPeoplePage}/>
+        <Route path="/movies/watchlist" component={WatchlistPage} />
+        <Route path="/people/:id" component={PersonPage} />
+        <Route path="/list/:id" component={ListPage} />
+        <Route path="/people" exact component={PopularPeoplePage} />
         <Footer />
       </BrowserRouter>
     </div>
